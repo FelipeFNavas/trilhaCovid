694d4330d59b79afff448fbd9983fa1b71dbf8c6
Felipe Navas <felipefnavas@gmail.com>
2021-12-07 23:54:35 -0300

    feat: salvando as questoes no banco e casas especiais habilitadas

--- ajax/updateMatches.php
+++ ajax/updateMatches.php
@@ -1,39 +1,31 @@
-
 <?php
-
+header('Access-Control-Allow-Origin: *');
 // Definindo Servidor, Nome de Usuário, Senha e Banco de Dados
 $servername = "localhost";
-$username = 'id17646567_felipe';
-$password = 'Ol9j$HBH)A9kP0@b';
-$database = 'id17646567_trilhacovid';
+$username = 'id17945054_admin';
+$password = 'UchihaItachi42!';
+$database = 'id17945054_trilhacovid';
 
-
-// Criando a ConexÃ£o
+//Criando a ConexÃ£o
 $connection = new mysqli($servername, $username, $password, $database);
-
-if(isset($_GET['id_match']) && isset($_GET['pontuacao'])){
-
-     //Lightly sanitize the GET's to prevent SQL injections and possible XSS attacks
+ if(isset($_GET['id_player']) && isset($_GET['score']) && isset($_GET['id_match'])){
+      //Lightly sanitize the GET's to prevent SQL injections and possible XSS attacks
+     $id_player = $_GET['id_player'];
+     $score = $_GET['score'];
      $id_match = $_GET['id_match'];
-     $pontuacao = $_GET['pontuacao'];
 
-     $query = mysqli_query($connection, "UPDATE matches set pontuacao = '{$pontuacao}' where id = '{$id_match}'");
-
+     $query = mysqli_query($connection, "UPDATE matches SET score = '{$score}' WHERE (id_match = '{$id_match}' and id_player = '{$id_player}')");
      if(!$query){
+          echo($query);
+          echo 'There was a problem saving your account. Please try again later.';
+    		die("QUERY FAILED" . mysqli_error($connection));
+    	}else{
+          echo 'Congrats!';
+     }
 
-       echo 'There was a problem saving your account. Please try again later.';
-   		 die("QUERY FAILED" . mysqli_error($connection));
-
-   	}else{
-
-       echo 'Congrats!';
-
-    }
-
-}else{
+ }else{
      echo 'Your name or score wasnt passed in the request. Make sure you add ?name=NAME_HERE&score=1337 to the tags.';
-}
-
+  }
 
 mysqli_close($connection);
 ?>
