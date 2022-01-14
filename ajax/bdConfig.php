30aa83a5b4b2f162637568d486d2c302031c7773
Felipe Navas <felipefnavas@gmail.com>
2022-01-14 00:53:52 -0300

    refactor/fix: alteracao nas cores de casas do tabuleiro/correcao do bug que fazia o jogador caminhar quando a maquina cai em uma casa especial

---
+++ ajax/bdConfig.php
@@ -0,0 +1,8 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+$servername = "localhost";
+$username = 'id17945054_admin';
+$password = 'UchihaItachi42!';
+$database = 'id17945054_trilhacovid';
+?>
---
+++ ajax/getRanking.php
@@ -0,0 +1,45 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+include('bdConfig.php');
+
+$connection = new mysqli($servername, $username, $password, $database);
+
+$query = mysqli_query( $connection, 
+    "SELECT id_match, players.name, sum(score) as soma, count(id_match)as qtd FROM `matches` 
+    inner join players on matches.id_player = players.id_player
+    Where score > 0
+    group by players.id_player
+    order by soma DESC, players.name"
+);
+
+$name = '';
+$score = '';
+$qtd_partida = '';
+$final = '';
+while ($rows = mysqli_fetch_array($query)) {
+
+    $name = substr($rows['name'], 0, 10);
+    if (strlen($name) < 10) {
+        $diff = 10 - strlen($name);
+        $name .= str_repeat(" ", $diff);
+    }
+    $score = substr($rows['soma'], 0, 4);
+    if (strlen($score) < 4) {
+        $diff = 4 - strlen($score);
+        $score = str_repeat("0", $diff) . $score;
+    }
+    $qtd_partida = substr($rows['qtd'], 0, 2);
+    if (strlen($qtd_partida) < 2) {
+        $diff = 2 - strlen($qtd_partida);
+        $qtd_partida = str_repeat("0", $diff) . $qtd_partida;
+    }
+    $final .= "$name        $score     $qtd_partida\n";
+
+}
+
+echo '<pre>';
+echo "\n";
+print_r($final);
+
+mysqli_close($connection);
