8ffb8b33a930beb40a61963fec0f6ae67d1c39e2
Felipe Navas <felipefnavas@gmail.com>
2022-01-15 20:27:20 -0300

    fix: correcoes na tela de vencedor e pontuacao

--- ajax/getRanking.php
+++ ajax/getRanking.php
@@ -19,24 +19,34 @@ $qtd_partida = '';
 $final = '';
 while ($rows = mysqli_fetch_array($query)) {
 
-    $name = substr($rows['name'], 0, 10);
-    if (strlen($name) < 10) {
-        $diff = 10 - strlen($name);
-        $name .= str_repeat(" ", $diff);
-    }
-    $score = substr($rows['soma'], 0, 4);
-    if (strlen($score) < 4) {
-        $diff = 4 - strlen($score);
-        $score = str_repeat("0", $diff) . $score;
-    }
-    $qtd_partida = substr($rows['qtd'], 0, 2);
-    if (strlen($qtd_partida) < 2) {
-        $diff = 2 - strlen($qtd_partida);
-        $qtd_partida = str_repeat("0", $diff) . $qtd_partida;
-    }
-    $final .= "$name        $score     $qtd_partida\n";
+    $name .= substr($rows['name'], 0, 15);
+    $name .= "\n";
+
+    $score .= substr($rows['soma'], 0, 4);
+    $score .= "\n";
+
+    $qtd_partida .= substr($rows['qtd'], 0, 2);
+    $qtd_partida .= "\n";
+    // if (strlen($name) < 10) {
+    //     $diff = 10 - strlen($name);
+    //     $name .= str_repeat(" ", $diff);
+    // }
+    // $score = substr($rows['soma'], 0, 4);
+    // if (strlen($score) < 4) {
+    //     $diff = 4 - strlen($score);
+    //     $score = str_repeat("0", $diff) . $score;
+    // }
+    // $qtd_partida = substr($rows['qtd'], 0, 2);
+    // if (strlen($qtd_partida) < 2) {
+    //     $diff = 2 - strlen($qtd_partida);
+    //     $qtd_partida = str_repeat("0", $diff) . $qtd_partida;
+    // }
+    // $final .= "$name        $score     $qtd_partida\n";
+
+    // $final .= "$name\n";
 
 }
+$final = $name . "|" . $score . "|" . $qtd_partida;
 
 echo '<pre>';
 echo "\n";
